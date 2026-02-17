073fb4aa730adc8af6dadc54a9d477556706aa99
CarinaBoiangiu <carina.boiangiu10@gmail.com>
2026-02-17 16:42:52 +0200

    init

---
+++ src/test/java/com/details/FlowerShop/FlowerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.details.FlowerShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
